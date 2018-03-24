ac468b0b713d1f85b2cec018031aba93aeeb924a
Darren Lim <darrenlzh@gmail.com>
2018-03-24 02:36:52 -0400

    Update angular to use sass

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -16,13 +16,15 @@ import { ValidateService } from './services/validate.service';
 import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 import { AuthGuard } from './guards/auth.guard';
+import { CatalogComponent } from './components/catalog/catalog.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path:  'catalog', component: CatalogComponent, canActivate: [AuthGuard] }
 ]
 
 @NgModule({
@@ -33,7 +35,8 @@ const appRoutes: Routes = [
     RegisterComponent,
     HomeComponent,
     DashboardComponent,
-    ProfileComponent
+    ProfileComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
